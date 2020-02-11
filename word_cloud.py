a573e743c3e62b0629b3a89b60d42898fa661317
Chris <chrishaining@yahoo.co.uk>
2020-02-11 18:14:05 +0000

    adds numbers cloud

--- word_cloud.py
+++ word_cloud.py
@@ -10,6 +10,9 @@ title1 = "The Iliad"
 text2 = open("odyssey.txt", "r").read().lower()
 title2 = "The Odyssey"
 
+text3 = "ten, ten, ten, ten, ten, ten, ten, ten, ten, ten, nine, nine, nine, nine, nine, nine, nine, nine, nine, eight, eight, eight, eight, eight, eight, eight, eight, seven, seven, seven, seven, seven, seven, seven, six, six, six, six, six, six, five, five, five, five, five, four, four, four, four, three, three, three, two, two, one"
+title3 = "Numbers"
+
 def create_word_cloud(string, title):
    maskArray = np.array(Image.open("cloud.png"))
    cloud = WordCloud(background_color = "white", max_words = 200, mask = maskArray, stopwords = set(STOPWORDS))
@@ -18,3 +21,5 @@ def create_word_cloud(string, title):
    cloud.to_file("%swordCloud.png" % clean_title)
 create_word_cloud(text1, title1)
 create_word_cloud(text2, title2)
+create_word_cloud(text3, title3)
+
